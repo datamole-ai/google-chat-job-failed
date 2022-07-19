eb5e03cf9c4b5bd82dd0e1dfb920ab272bada8c2
Dmitrii Dumih <dmitrii.dumih@datamole.cz>
2022-07-19 13:55:05 +0200

    rewrote action in python

---
+++ action.py
@@ -0,0 +1,34 @@
+import os
+import urllib3
+import json
+
+needs_context = os.getenv('NEEDS_CONTEXT')
+google_chat_webhook = os.getenv('GOOGLE_CHAT_WEBHOOK')
+github_link = os.getenv('GITHUB_LINK')
+head = os.getenv('HEAD')
+repo_name = os.getenv('REPO_NAME')
+
+failed_list = []
+
+print(type(needs_context))
+for job_name in needs_context:
+    job = needs_context.get(job_name)
+    if job.get('result') == 'failure':
+        failed_list.append(job_name)
+
+if len(failed_list) == 0:
+    exit(0)
+else:
+    text = 'Failed jobs: <br><b>' + '<br>'.join(map(str, failed_list)) + '</b>'
+
+text_string = {"cards": [{"header": {"title":"<b>JOBS FAILED</b>","subtitle": head},"sections":[{"widgets":[{"textParagraph":{"text": text}},{"buttons":[{"textButton":{"text":"VIEW RUN","onClick":{"openLink":{"url": github_link}}}}]}]}]}]}
+message = json.dumps(text_string)
+print(message)
+
+http = urllib3.PoolManager()
+r = http.request(
+    'POST',
+    google_chat_webhook,
+    headers={'Content-Type': 'application/json'},
+    body=message
+  )
